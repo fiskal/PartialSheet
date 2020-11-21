970a08d58c1ba5523936fa8483cdfb8574bb7448
Fiskal <fiskal@M1.local>
2020-11-20 19:05:05 -0800

    chore: update to swift-tools 5.3

--- Package.swift
+++ Package.swift
@@ -1,14 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PartialSheet",
-	platforms: [
-	  .iOS(.v13)
-	],
-    products: [
+	products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "PartialSheet",
@@ -29,3 +26,5 @@ let package = Package(
             dependencies: ["PartialSheet"]),
     ]
 )
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
